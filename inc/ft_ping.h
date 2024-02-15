26ce54c7224315488d1f40e6b0b4472ff88c2231
Naerhy <naerhy@protonmail.com>
2024-02-16 00:49:28 +0100

    Implement naive printing of argv hostname into readable ip address

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -1,6 +1,12 @@
 #ifndef FT_PING_H
 #define FT_PING_H
 
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
 #endif
--- src/ft_ping.c
+++ src/ft_ping.c
@@ -1,9 +1,37 @@
 #include "ft_ping.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
+	struct addrinfo hints;
+	struct addrinfo* addrs;
+	struct sockaddr_in* addr;
+	char ipstr[INET_ADDRSTRLEN];
+
+	if (argc < 2)
+	{
+		printf("ft_ping: missing host operand\n");
+		return 1;
+	}
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_RAW;
+	if (getaddrinfo(*(argv + 1), NULL, &hints, &addrs))
+	{
+		printf("ft_ping: unknown host\n");
+		return 1;
+	}
+	for (struct addrinfo* p = addrs; p; p = p->ai_next)
+	{
+		addr = (struct sockaddr_in*)p->ai_addr;
+		inet_ntop(p->ai_family, (void const*)(&addr->sin_addr), ipstr, INET_ADDRSTRLEN);
+		printf("%s\n", ipstr);
+	}
+	freeaddrinfo(addrs);
+	/*
 	int sockfd;
 
-	(void)sockfd;
+	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &optvalue, sizeof(optvalue)))
+	*/
 	return 0;
 }
