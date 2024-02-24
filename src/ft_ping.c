b012f8c9f6c0c47af9eab43b0c540b4e3075e827
Naerhy <naerhy@protonmail.com>
2024-02-24 11:15:10 +0100

    Move socket initialization to its own function

--- src/ft_ping.c
+++ src/ft_ping.c
@@ -39,6 +39,26 @@ static int exit_ping(char const* err)
 	return err ? 1 : 0;
 }
 
+static int init_socket(char const* host)
+{
+	struct addrinfo hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_RAW;
+	if (getaddrinfo(host, NULL, &hints, &info->addrs))
+		return -1;
+	info->sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (info->sockfd == -1)
+		return -2;
+	// TODO: handle ttl flag in argv + validate max value
+	if (setsockopt(info->sockfd, IPPROTO_IP, IP_TTL, &info->ttl, sizeof(info->ttl)) == -1)
+		return -3;
+	info->addr = (struct sockaddr_in*)info->addrs->ai_addr;
+	inet_ntop(info->addrs->ai_family, &info->addr->sin_addr, info->ipstr, sizeof(info->ipstr));
+	return 1;
+}
+
 static int init_info(void)
 {
 	info = malloc(sizeof(Info));
@@ -57,31 +77,22 @@ static int init_info(void)
 
 int main(int argc, char** argv)
 {
-	struct addrinfo hints;
+	int sockinit;
 
 	if (argc < 2)
 		return exit_ping("ft_ping: missing host operand\n");
 	if (!init_info())
 		return exit_ping("ft_ping: unable to allocate memory\n");
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_RAW;
-	if (getaddrinfo(*(argv + 1), NULL, &hints, &info->addrs))
-		return exit_ping("ft_ping: unknown host\n");
-	info->sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (info->sockfd == -1)
-		return exit_ping("ft_ping: unable to create socket\n");
-	// TODO: handle ttl flag in argv + validate max value
-	if (setsockopt(info->sockfd, IPPROTO_IP, IP_TTL, &info->ttl, sizeof(info->ttl)) == -1)
-		return exit_ping("ft_ping: unable to set socket option\n");
-	info->addr = (struct sockaddr_in*)info->addrs->ai_addr;
-	inet_ntop(info->addrs->ai_family, &info->addr->sin_addr, info->ipstr, sizeof(info->ipstr));
+	sockinit = init_socket(argv[1]);
+	if (sockinit != 1)
+		return exit_ping(sockinit == -1 ? "ft_ping: unknown host\n"
+				: sockinit == -2 ? "ft_ping: unable to create socket\n"
+				: "ft_ping: unable to set socket option\n");
 	printf("PING %s (%s): 56 data bytes\n", *(argv + 1), info->ipstr);
 	signal(SIGALRM, handle_signals);
 	signal(SIGINT, handle_signals);
-	// invert raise and recvping to avoid missing first recv?
 	raise(SIGALRM);
 	recvping();
-	print_stats(*(argv + 1));
+	print_stats(argv[1]);
 	return exit_ping(NULL);
 }
